c319ce4428e68e7ee41a8bd2e00052bac14b535e
Lance Anderson <lance.a.585@gmail.com>
2024-01-28 12:54:57 -0800

    UPDATE Navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,3 +1,76 @@
+import Image from "next/image";
+import navLogo from "/public/assets/icons8-image-placeholder-96.png";
+import Link from "next/link";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+
 export default function Navbar() {
-  return <div className=" fixed w-full  h-20">Navbar</div>;
+  return (
+    <div className=" fixed w-full  h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full px-2 2xl:px-16">
+        <Image src={navLogo} alt="/" width="90" />
+        <div>
+          <ul className=" hidden md:flex">
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Projects
+              </li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Contact
+              </li>
+            </Link>
+          </ul>
+          <div className="md:hidden">
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+        <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
+          <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+            <div>
+              <div className="flex w-full items-center justify-between">
+                <Image src={navLogo} alt="/" width={87} height={35} />
+                <div className=" rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer">
+                  <AiOutlineClose />
+                </div>
+              </div>
+              <div className="border-b border-gray-300 my-4">
+                <p className="w-[85%] md:w-[90%] py-4">
+                  Let's build something amazing together.
+                </p>
+              </div>
+              <div>
+                <ul>
+                  <Link href="/">
+                    <li className=" py-4 text-sm">Home</li>
+                  </Link>
+                  <Link href="/">
+                    <li className=" py-4 text-sm">About</li>
+                  </Link>
+                  <Link href="/">
+                    <li className=" py-4 text-sm">Skills</li>
+                  </Link>
+                  <Link href="/">
+                    <li className=" py-4 text-sm">Projects</li>
+                  </Link>
+                  <Link href="/">
+                    <li className=" py-4 text-sm">Contact</li>
+                  </Link>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
