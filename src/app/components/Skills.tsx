c6387318d6691e10751e9faa2896f19c81b389e3
Lance Anderson <lance.a.585@gmail.com>
2024-01-31 19:45:39 -0800

    UPDATE formatting for Skills

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -1,7 +1,13 @@
 export default function Skills() {
   return (
     <div className=" w-full lg:h-screen p-2">
-      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full"></div>
+      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
+        <p className=" text-xl tracking-widest uppercase text-[#5651e5]">
+          Skills
+        </p>
+        <h2 className=" py-4">My skills to pay the bills</h2>
+        <div className=" grid md:grid-cols-2 lg:grid-cols-4 gap"></div>
+      </div>
     </div>
   );
 }
