ff3b9072fdf1c06ca504a512c34df036b0e9ee6f
Lance Anderson <lance.a.585@gmail.com>
2024-01-31 19:39:36 -0800

    ADD - start a skills component

---
+++ src/app/components/Skills.tsx
@@ -0,0 +1,7 @@
+export default function Skills() {
+  return (
+    <div className=" w-full lg:h-screen p-2">
+      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full"></div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import About from "./components/About";
 import Main from "./components/Main";
+import Skills from "./components/Skills";
 
 export default function Home() {
   return (
     <>
       <Main />
       <About />
+      <Skills />
     </>
   );
 }
