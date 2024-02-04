9e280bb771e411c9b86a77f251f5eac7bb0402f7
Lance Anderson <lance.a.585@gmail.com>
2024-02-03 19:08:37 -0800

    UPDATE Contact - updated styling of contact page

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 import contactImg from "/public/assets/nordwood-themes-q8U1YgBaRQk-unsplash.jpg";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { AiOutlineMail } from "react-icons/ai";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import Link from "next/link";
+import { HiOutlineChevronDoubleUp } from "react-icons/hi";
 
 export default function Contact() {
   return (
@@ -20,10 +25,90 @@ export default function Contact() {
                   alt="/"
                 />
               </div>
+              <div>
+                <h2 className=" py-2">Lance Anderson</h2>
+                <p>Web & App Developer</p>
+                <p className="py-4 ">
+                  This is some fake text for my contact section. This will be
+                  replaced.
+                </p>
+              </div>
+              <div>
+                <p className=" uppercase pt-8">Connect With Me.</p>
+              </div>
+              <div className=" flex items-center justify-between py-4">
+                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                  <FaLinkedinIn />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                  <FaGithub />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                  <AiOutlineMail />
+                </div>
+                <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+                  <BsFillPersonLinesFill />
+                </div>
+              </div>
             </div>
           </div>
 
           {/* right */}
+          <div className="col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4">
+            <div className="p-4">
+              <form>
+                <div className="grid md:grid-cols-2 gap-4 w-full py-2">
+                  <div className="flex flex-col">
+                    <label className=" uppercase text-sm py-2">Name</label>
+                    <input
+                      className=" border-2 rounded-lg p-3 flex border-gray-300"
+                      type="text"
+                    />
+                  </div>
+                  <div className="flex flex-col">
+                    <label className=" uppercase text-sm py-2">
+                      Phone Number
+                    </label>
+                    <input
+                      className=" border-2 rounded-lg p-3 flex border-gray-300"
+                      type="text"
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className=" uppercase text-sm py-2">Email</label>
+                  <input
+                    className=" border-2 rounded-lg p-3 flex border-gray-300"
+                    type="email"
+                  />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className=" uppercase text-sm py-2">Subject</label>
+                  <input
+                    className=" border-2 rounded-lg p-3 flex border-gray-300"
+                    type="text"
+                  />
+                </div>
+                <div className="flex flex-col py-2">
+                  <label className=" uppercase text-sm py-2">Message</label>
+                  <textarea
+                    className=" border-2 rounded-lg p-3 border-gray-300"
+                    rows={10}
+                  ></textarea>
+                </div>
+                <button className="w-full p-4 text-gray-100 mt-4">
+                  Send Message
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div className="flex justify-center py-12">
+          <Link href="/">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <HiOutlineChevronDoubleUp className=" text-[#5651e5]" size={30} />
+            </div>
+          </Link>
         </div>
       </div>
     </div>
