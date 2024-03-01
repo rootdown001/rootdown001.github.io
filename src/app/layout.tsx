599247249dfbbddf506d25c96a23349dc2924787
Lance Anderson <lance.a.585@gmail.com>
2024-02-29 18:34:53 -0800

    UPDATE speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "Lance | React Developer",
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body>
         <Navbar />
         <div>{children}</div>
+        <SpeedInsights />
       </body>
     </html>
   );
